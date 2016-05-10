1c2fd3ef98ed85e0758a25fc41a64ce16c6e2597
abisheka <abishek91@gmail.com>
2016-05-10 17:46:18 +0530

    Revert "Abishek/Hanisha | #1140 | Visit matcher for offline"
    This reverts commit f6464285879aa46658ba295171eb6082b0f74324.

--- api/src/main/java/org/bahmni/module/bahmniOfflineSync/OfflineVisitMatcher.java
+++ api/src/main/java/org/bahmni/module/bahmniOfflineSync/OfflineVisitMatcher.java
@@ -2,43 +2,36 @@ package org.bahmni.module.bahmniOfflineSync;
 
 import org.openmrs.Patient;
 import org.openmrs.Visit;
-import org.openmrs.api.VisitService;
-import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniEncounterTransaction;
-import org.openmrs.module.bahmniemrapi.encountertransaction.service.VisitIdentificationHelper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
+import org.openmrs.VisitType;
+import org.openmrs.module.bahmniemrapi.encountertransaction.service.VisitMatcher;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.List;
 
-@Primary
 @Component
-public class OfflineVisitMatcher extends VisitIdentificationHelper{
-
-    @Autowired
-    public OfflineVisitMatcher(VisitService visitService) {
-        super(visitService);
+public class OfflineVisitMatcher implements VisitMatcher {
+    @Override
+    public Visit getVisitFor(Patient patient, String s, Date date, Date date1, Date date2) {
+        return null;
     }
 
     @Override
-    public void createOrStretchVisit(BahmniEncounterTransaction bahmniEncounterTransaction, Patient patient, Date visitStartDate, Date visitEndDate) {
-        String visitTypeForNewVisit = bahmniEncounterTransaction.getVisitType();
-        Date orderDate = bahmniEncounterTransaction.getEncounterDateTime();
-        List<Visit> activeVisits = visitService.getActiveVisitsByPatient(patient);
-        if (matchingVisitsFound(activeVisits)) {
-            if(activeVisits.get(0).getVisitType().getName().equals(visitTypeForNewVisit)) {
-                Visit matchingVisit = getVisit(orderDate, activeVisits);
-                stretchVisits(orderDate, matchingVisit);
-                return;
-            }
-            else {
-                visitService.endVisit(activeVisits.get(0), new Date());
-            }
-        }
-        createNewVisit(patient, orderDate, visitTypeForNewVisit, visitStartDate, visitEndDate);
+    public Visit getVisitFor(Patient patient, String s, Date date) {
+        return null;
     }
 
+    @Override
+    public boolean hasActiveVisit(Patient patient) {
+        return false;
+    }
 
+    @Override
+    public Visit createNewVisit(Patient patient, Date date, String s, Date date1, Date date2) {
+        return null;
+    }
 
+    @Override
+    public VisitType getVisitTypeByName(String s) {
+        return null;
+    }
 }

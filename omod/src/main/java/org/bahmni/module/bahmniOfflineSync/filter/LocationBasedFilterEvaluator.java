9e5623c0c8eaeadd1caebe81286993df8421b611
Dé <rahul080327@gmail.com>
2016-08-26 14:20:11 +0530

    Rahul/Santosh | Enforce type checks and formats

--- omod/src/main/java/org/bahmni/module/bahmniOfflineSync/filter/LocationBasedFilterEvaluator.java
+++ omod/src/main/java/org/bahmni/module/bahmniOfflineSync/filter/LocationBasedFilterEvaluator.java
@@ -7,15 +7,13 @@ import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.addresshierarchy.AddressHierarchyEntry;
 import org.openmrs.module.addresshierarchy.service.AddressHierarchyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LocationBasedFilterEvaluator implements FilterEvaluator{
+public class LocationBasedFilterEvaluator implements FilterEvaluator {
 
     private static final String ATTRIBUTE_TYPE_NAME = "addressCode";
 
@@ -31,43 +29,44 @@ public class LocationBasedFilterEvaluator implements FilterEvaluator{
     private String evaluateFilterForPatient(String uuid) {
         String patientFilter = "";
         Patient patient = patientService.getPatientByUuid(uuid);
-        if(patient!=null && patient.getAttribute(ATTRIBUTE_TYPE_NAME)!=null) {
+
+        if (patient != null && patient.getAttribute(ATTRIBUTE_TYPE_NAME) != null) {
             patientFilter = patient.getAttribute(ATTRIBUTE_TYPE_NAME).getValue();
         }
+
         return patientFilter;
     }
-
 
     private String evaluateFilterForEncounter(String uuid) {
         String filter = "";
         Encounter encounter = encounterService.getEncounterByUuid(uuid);
-        if(encounter!=null)
+        if (encounter != null)
             filter = evaluateFilterForPatient(encounter.getPatient().getUuid());
         return filter;
-
     }
-
 
     private String evaluateFilterForAddressHierarchy(String uuid) {
         String addressHierarchyFilter = "";
         AddressHierarchyService addressHierarchyService = Context.getService(AddressHierarchyService.class);
         AddressHierarchyEntry addressHierarchyEntry = addressHierarchyService.getAddressHierarchyEntryByUuid(uuid);
-        if (addressHierarchyEntry!=null && addressHierarchyEntry.getLevel() != null && addressHierarchyEntry.getLevel().getId() > 3) {
-            addressHierarchyFilter =  addressHierarchyEntry.getUserGeneratedId();
+        if (addressHierarchyEntry != null && addressHierarchyEntry.getLevel() != null && addressHierarchyEntry.getLevel().getId() > 3) {
+            addressHierarchyFilter = addressHierarchyEntry.getUserGeneratedId();
         }
         return addressHierarchyFilter;
     }
 
     @Override
-    public Map<String, String> getFilterForDevice(String providerUuid , String locationUuid) {
-        Map categoryFilterMap = new HashMap();
+    public Map<String, String> getFilterForDevice(String providerUuid, String locationUuid) {
+        Map<String, String> categoryFilterMap = new HashMap();
         AddressHierarchyService addressHierarchyService = Context.getService(AddressHierarchyService.class);
         AddressHierarchyEntry addressHierarchyEntry = addressHierarchyService.getAddressHierarchyEntryByUuid(locationUuid);
         String userGeneratedId = null;
-        if(addressHierarchyEntry!=null)
+
+        if (addressHierarchyEntry != null)
             userGeneratedId = addressHierarchyEntry.getUserGeneratedId();
+
         categoryFilterMap.put("TransactionalData", userGeneratedId);
-        categoryFilterMap.put("AddressHierarchy",getCatchmentNumberForAddressHierarchy(addressHierarchyEntry));
+        categoryFilterMap.put("AddressHierarchy", getCatchmentNumberForAddressHierarchy(addressHierarchyEntry));
         categoryFilterMap.put("ParentAddressHierarchy", null);
         categoryFilterMap.put("offline-concepts", null);
 
@@ -77,34 +76,38 @@ public class LocationBasedFilterEvaluator implements FilterEvaluator{
     @Override
     public List<String> getEventCategoriesList() {
         List<String> eventCategoryList = new ArrayList();
+
         eventCategoryList.add("TransactionalData");
         eventCategoryList.add("AddressHierarchy");
         eventCategoryList.add("ParentAddressHierarchy");
         eventCategoryList.add("offline-concepts");
+
         return eventCategoryList;
     }
 
-
-    private String getCatchmentNumberForAddressHierarchy (AddressHierarchyEntry addressHierarchyEntry){
+    private String getCatchmentNumberForAddressHierarchy(AddressHierarchyEntry addressHierarchyEntry) {
         String filterForAddressHierarchy = null;
-        while(addressHierarchyEntry.getParent()!=null) {
-            if(addressHierarchyEntry.getUserGeneratedId().length() == 6){
+
+        while (addressHierarchyEntry.getParent() != null) {
+            if (addressHierarchyEntry.getUserGeneratedId().length() == 6) {
                 filterForAddressHierarchy = addressHierarchyEntry.getUserGeneratedId();
                 break;
             }
             addressHierarchyEntry = addressHierarchyEntry.getParent();
         }
+
         return filterForAddressHierarchy;
     }
 
     @Override
-    public  String evaluateFilter(String uuid, String category) {
+    public String evaluateFilter(String uuid, String category) {
         String filter = "";
-        if(category.equalsIgnoreCase("Patient"))
+
+        if (category.equalsIgnoreCase("Patient"))
             filter = evaluateFilterForPatient(uuid);
-        else if(category.equalsIgnoreCase("Encounter") || category.equalsIgnoreCase("SHREncounter"))
-            filter =  evaluateFilterForEncounter(uuid);
-        else if(category.equalsIgnoreCase("AddressHierarchy"))
+        else if (category.equalsIgnoreCase("Encounter") || category.equalsIgnoreCase("SHREncounter"))
+            filter = evaluateFilterForEncounter(uuid);
+        else if (category.equalsIgnoreCase("AddressHierarchy"))
             filter = evaluateFilterForAddressHierarchy(uuid);
 
         return filter;

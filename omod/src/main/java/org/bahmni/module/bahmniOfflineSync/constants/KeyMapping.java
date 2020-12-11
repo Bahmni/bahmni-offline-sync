deb79e10427588ebeda9cb61bcc673ec1b141dbc
gopikrishna-yaramothu <gopikrishna.y@thoughtworks.com>
2020-12-11 07:59:43 +0200

    Gopi,Shriram | Changes Related To Zipping of AddressHierarchy and OfflineConcepts (cherry picked from commit 54be5d5ea614296b4b50b53834cdb97a0e720b69)

---
+++ omod/src/main/java/org/bahmni/module/bahmniOfflineSync/constants/KeyMapping.java
@@ -0,0 +1,21 @@
+package org.bahmni.module.bahmniOfflineSync.constants;
+
+public class KeyMapping {
+
+    public static final String AddressHierarchyEntry_ID = "\"addressHierarchyEntryId\":";
+    public static final String Name = "\"name\":\"";
+    public static final String Level_ID= " \"levelId\":";
+    public static final String AddressHierarchyLevel= "\"addressHierarchyLevel\": {";
+    public static final String AddressHierarchyLevelID= "\"levelId\":";
+    public static final String AddressHierarchyLevelName= "\"name\":\"";
+    public static final String AddressHierarchyLevelParent_ID= "\"parentLevelId\":";
+    public static final String AddressHierarchyLevel_AddressFiled= "\"addressField\":\"";
+    public static final String AddressHierarchyLevel_Required= "\"required\":";
+    public static final String AddressHierarchyLevel_UUID= "\"uuid\":\"";
+    public static final String AddressHierarchyLevel_ID = "\"id\":";
+    public static final String Parent_ID= " \"parentId\":";
+    public static final String UserGenerated_ID= " \"userGeneratedId\":\"";
+    public static final String UUID= " \"uuid\":\"";
+    public static final String UserName = "userName";
+    public static final String Password = "password";
+}
